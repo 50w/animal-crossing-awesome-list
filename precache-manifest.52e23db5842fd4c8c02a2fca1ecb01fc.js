34154314f3d562e5ce2195f6bc0af5728b51a5eb
NickLewanowicz <nicklewanowicz@gmail.com>
2020-05-05 17:24:15 -0400

    Updates

---
+++ precache-manifest.52e23db5842fd4c8c02a2fca1ecb01fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8a5aecd9bac76b403ef",
+    "url": "/static/css/main.e70248da.chunk.css"
+  },
+  {
+    "revision": "d8a5aecd9bac76b403ef",
+    "url": "/static/js/main.d8a5aecd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "1ea6c3696cc533271548",
+    "url": "/static/js/2.1ea6c369.chunk.js"
+  },
+  {
+    "revision": "5d6e7a349eed80c163338b31e42d9e51",
+    "url": "/index.html"
+  }
+];
